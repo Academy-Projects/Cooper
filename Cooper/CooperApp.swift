49b19152eff1afa0caa398b3ba0e2d2c84224290
kevin marinho <kevinmarinho@kevins-MacBook-Pro.local>
2022-08-15 19:09:55 -0400

    first commit

---
+++ Cooper/CooperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CooperApp.swift
+//  Cooper
+//
+//  Created by kevin marinho on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
