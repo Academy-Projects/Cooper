cdfb1d326d9f36d2072f64bb5773f9652cac8697
Matheus-C-S-Azevedo <matheus.costa.s.azevedo@gmail.com>
2022-08-18 16:52:50 -0400

    mudanca de layer funcional quase funcional, precisa de ajustes

--- Cooper/DrawnView.swift
+++ Cooper/DrawnView.swift
@@ -13,50 +13,84 @@ struct DrawnView: View {
     
     @State var positions : [CGPoint] = [
         CGPoint(x: 100, y: 100),
-        CGPoint(x: 300, y: 300)//,
-//        CGPoint(x: 0, y: 0),
-//        CGPoint(x: 0, y: 0),
-//        CGPoint(x: 0, y: 0)
+        CGPoint(x: 300, y: 300),
+        CGPoint(x: 0, y: 0),
+        CGPoint(x: 0, y: 0),
+        CGPoint(x: 0, y: 0)
     ]
 
    // @State var pos: [CGFloat] = [50, 100, 150, 160]
     @State var MyImages: [UIImage] = [
         UIImage(named: "X")!,
-        UIImage(named: "Eyes")!//,
-//        UIImage(named: "Done!")!,
-//        UIImage(named: "Fire")!,
-//        UIImage(named: "Comment")!
+        UIImage(named: "Eyes")!,
+        UIImage(named: "Done!")!,
+        UIImage(named: "Fire")!,
+        UIImage(named: "Comment")!
     ]
     
     @State var imgTemporary: UIImage!
-//    @State var contador: Int = 0
-    @State var controle:Bool = false
+    @State var posTemporary:CGPoint =  CGPoint(x: 0, y: 0)
+    @State var begginGesture:Bool = true
+    
+    @State var nImagem:Int = 0
+    @State var isDraggin:Bool = false
     
     var body: some View{
         ZStack{
             Color(red: 255/255, green: 204/255, blue: 207/255).ignoresSafeArea(.all)
             
             ZStack(alignment: .bottomLeading){
-                Button("CLICK-ME"){
-                    Image(uiImage: MyImages[0])
-//                        .frame(width: 50, height: 50, alignment: .center)
-//                    loadImages(imageArray: MyImages, posArray: positions)
-                    print("foi clicado")
+
+                ForEach(0 ..< MyImages.count, id: \.self){imageIdx in
+                    Image(uiImage: MyImages[imageIdx])
+                        .position(positions[imageIdx])
+                        .onTapGesture {
+                            if (imageIdx != (MyImages.count - 1)) {
+                                posTemporary = positions[MyImages.count - 1]
+                                positions[MyImages.count - 1] = positions[imageIdx]
+                                positions[imageIdx] = posTemporary
+
+                                imgTemporary = MyImages[MyImages.count - 1]
+                                MyImages[MyImages.count - 1] = MyImages[imageIdx]
+                                MyImages[imageIdx] = imgTemporary
+                            }
+                        }
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ value in
+                                    isDraggin = true
+                                    positions[imageIdx] = value.location
+                                    
+                                })
+                                .onEnded({ value in
+                                    
+                                })
+                        )
+                    }
                 }
+            VStack(){
+                Button("CLICK-ME"){
+                    MyImages += [UIImage(named: "X")!]
+                    positions.append(CGPoint(x: 0, y: 0))
+                    print(MyImages)
+                    print(positions)
+                }.frame(width: 200, height: 50, alignment: .center)
+                .foregroundColor(.white)
+                .background(.blue)
+            
+                Button("Mudar"){
+                    imgTemporary = MyImages[1]
+                    MyImages[1] = MyImages[0]
+                    MyImages[0] = imgTemporary
+                    print(MyImages.count)
+                }.frame(width: 200, height: 50, alignment: .center)
+                .foregroundColor(.white)
+                .background(.blue)
+            }
+            
             }
         }
     }
-}
-
-func loadImages(imageArray:[UIImage], posArray:[CGPoint]){
-    ForEach {
-        Image(uiImage: imageArray[i])
-            .frame(width: 150, height: 50, alignment: .center)
-//            .offset(x: 150, y: 150)
-//            .position(posArray[i])
-        print("funcao executada")
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
