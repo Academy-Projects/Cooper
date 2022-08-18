a055cca1fdd57c7db2c82d01cb4a943e8e65f446
Matheus-C-S-Azevedo <matheus.costa.s.azevedo@gmail.com>
2022-08-18 13:36:17 -0400

    testando coisas

--- Cooper/DrawnView.swift
+++ Cooper/DrawnView.swift
@@ -29,7 +29,7 @@ struct DrawnView: View {
     ]
     
     @State var imgTemporary: UIImage!
-    @State var contador: Int = 0
+//    @State var contador: Int = 0
     @State var controle:Bool = false
     
     var body: some View{
@@ -37,87 +37,27 @@ struct DrawnView: View {
             Color(red: 255/255, green: 204/255, blue: 207/255).ignoresSafeArea(.all)
             
             ZStack(alignment: .bottomLeading){
-                ForEach(0..<MyImages.count){imageIdx in
-                    Image(uiImage: MyImages[imageIdx])
-                        .position(positions[imageIdx])
-                        .gesture(
-                            DragGesture()
-                                .onChanged({ value in
-                                    positions[imageIdx] = value.location
-                                    if(controle == false){
-                                    imgTemporary = MyImages[imageIdx]
-                                    MyImages[imageIdx] = MyImages[imageIdx - 1]
-                                    MyImages[imageIdx - 1] = imgTemporary
-                                    controle = true
-                                    }
-//                                    idDragging = true
-                                })
-                                .onEnded({ value in
-//                                    position = .zero
-//                                    idDragging = false
-                                })
-                        )
+                Button("CLICK-ME"){
+                    Image(uiImage: MyImages[0])
+//                        .frame(width: 50, height: 50, alignment: .center)
+//                    loadImages(imageArray: MyImages, posArray: positions)
+                    print("foi clicado")
                 }
             }
-            .border(Color.black, width: 6)
-            
-//            Button("click"){
-//                Image(uiImage: MyImages[0])
-//                   .position(positions[0])
-//                   .gesture(
-//                       DragGesture()
-//                           .onChanged({ value in
-//                               positions[0] = value.location
-//                               MyImages[0] = MyImages[0]
-////                                    idDragging = true
-//                           })
-//                           .onEnded({ value in
-////                                    position = .zero
-////                                    idDragging = false
-//                           })
-//                   )
-//            }.frame(width: 300, height: 300, alignment: .center)
-            
         }
     }
 }
 
-//func lacofor () {
-//    @State var positions : [CGPoint] = [
-//        CGPoint(x: 0, y: 0),
-//        CGPoint(x: 0, y: 0)//,
-////        CGPoint(x: 0, y: 0),
-////        CGPoint(x: 0, y: 0),
-////        CGPoint(x: 0, y: 0)
-//    ]
-//
-//   // @State var pos: [CGFloat] = [50, 100, 150, 160]
-//    @State var MyImages: [UIImage] = [
-//        UIImage(named: "X")!,
-//        UIImage(named: "Eyes")!//,
-////        UIImage(named: "Done!")!,
-////        UIImage(named: "Fire")!,
-////        UIImage(named: "Comment")!
-//    ]
-//
-//
-//    ForEach(0..<MyImages.count){imageIdx in
-//        Image(uiImage: MyImages[imageIdx])
-//            .position(positions[imageIdx])
-//            .gesture(
-//                DragGesture()
-//                    .onChanged({ value in
-//                        positions[imageIdx] = value.location
-//                        MyImages[imageIdx] = MyImages[0]
-////                                    idDragging = true
-//                    })
-//                    .onEnded({ value in
-////                                    position = .zero
-////                                    idDragging = false
-//                    })
-//            )
-//    }
-//}
+func loadImages(imageArray:[UIImage], posArray:[CGPoint]){
+    ForEach {
+        Image(uiImage: imageArray[i])
+            .frame(width: 150, height: 50, alignment: .center)
+//            .offset(x: 150, y: 150)
+//            .position(posArray[i])
+        print("funcao executada")
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         DrawnView()
