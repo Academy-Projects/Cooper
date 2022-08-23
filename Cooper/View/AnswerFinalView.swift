82c077ed41030285f9ce9081c4d52af1eb261037
kevin marinho <kevindmarinho4321@gmail.com>
2022-08-23 13:30:05 -0400

    adicionando botao para tela de escolha

---
+++ Cooper/View/AnswerFinalView.swift
@@ -0,0 +1,17 @@
+//
+//  AnswerFinalView.swift
+//  Cooper
+//
+//  Created by kevin marinho on 23/08/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct AnswerFinalView: View {
+    var body: some View{
+        ZStack{
+           Text("VIEW DAS RESPOSTA")
+        }
+    }
+}
