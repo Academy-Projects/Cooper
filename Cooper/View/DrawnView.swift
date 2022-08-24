d2b1ce0f2563aed80a55f065ee7aea9f1ee66b76
kevin marinho <kevindmarinho4321@gmail.com>
2022-08-24 16:39:16 -0400

    add novos autolayouts

--- Cooper/View/DrawnView.swift
+++ Cooper/View/DrawnView.swift
@@ -25,7 +25,7 @@ struct DrawnView: View {
     @State var nImagem:Int = 0
     @State var isDraggin:Bool = false
     
-    @State var imagens:[String] = ["guardachuva", "chapeu", "Fire", "Done!"]
+    @State var imagens:[[String]] = [["guardachuva", "chapeu", "Fire"], ["X", "Eyes", "Done!"]]
     
     
     var body: some View{
@@ -33,49 +33,51 @@ struct DrawnView: View {
          HStack{
             ZStack{
                 SpriteView(scene: scene, isPaused: false)
-                    .padding(EdgeInsets(top: 4, leading: 14, bottom: 4, trailing: 14))
             }
+            .background(Color.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            //.overlay(RoundedRectangle(cornerRadius: 100).stroke(Color.black))
+            .background(RoundedRectangle(cornerRadius: 20)
+                .stroke()
+            )
+            .padding(EdgeInsets(top: 0, leading: 14, bottom: 0, trailing: 0))
+             
             VStack{
             Text("Clique nos cards que deseja usar \npara ilustrar a história")
                     .bold()
                     .multilineTextAlignment(.center)
+                    .padding()
             ScrollView{
                // Spacer()
-                HStack{
-                    ForEach(0..<2){imageidx in
-                        Button(action: {gameScene.createNewObj(image: imagens[imageidx])}){
-                            Image(imagens[imageidx])
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 161, height: 168, alignment: .center)
-                                .background(.gray)
-                                .cornerRadius(34.0)
-                                .overlay(RoundedRectangle(cornerRadius: 34).stroke(Color.black, lineWidth: 2))
-                        
-                        }
-                        //.frame(width: 101, height: 108)
-                        .padding(EdgeInsets(top: 56, leading: 14.5, bottom: 0, trailing: 9.6))
+                VStack{
+                    ForEach(0..<Int(imagens[0].count)){imageidx in
+                        HStack{
+                            Button(action: {gameScene.createNewObj(image: imagens[0][imageidx])}){
+                                Image(imagens[0][imageidx])
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 161, height: 168, alignment: .center)
+                                    .background(.gray)
+                                    .cornerRadius(35.0)
+                                    .overlay(RoundedRectangle(cornerRadius: 35).stroke(Color.black, lineWidth: 2))
+                            
+                            }
+                            Button(action: {gameScene.createNewObj(image: imagens[1][imageidx])}){
+                                Image(imagens[1][imageidx])
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 161, height: 168, alignment: .center)
+                                    .background(.gray)
+                                    .cornerRadius(34.0)
+                                    .overlay(RoundedRectangle(cornerRadius: 34).stroke(Color.black, lineWidth: 2))
+                            
+                            }
+                        }.padding(EdgeInsets(top: 56, leading: 14.5, bottom: 0, trailing: 9.6))
                     }
                 }
+            }
                // Spacer()
                // Spacer(minLength: 20)
-                HStack{
-                    ForEach(2..<4){imageidx in
-                        Button(action: {gameScene.createNewObj(image: imagens[imageidx])}){
-                            Image(imagens[imageidx])
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 161, height: 168, alignment: .center)
-                                .background(.gray)
-                                .cornerRadius(34.0)
-                                .overlay(RoundedRectangle(cornerRadius: 34).stroke(Color.black, lineWidth: 2))
-                        
-                        }
-                        //.frame(width: 101, height: 108)
-                        .padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 9.6))
-                    }
-                }
-            }.background(.gray)
                 NavigationLink(destination: AnswerFinalView(ilustrationScene: gameScene), label: {
                     Text("Done!")
                     .font(.system(size: 30))
@@ -85,8 +87,16 @@ struct DrawnView: View {
                                                 //.shadow(color: .gray, radius: 5, x: 0.5, y: 0.5)
                                                 .foregroundColor(Color(red: 45/255, green: 29/255, blue: 66/255))
                                                 .cornerRadius(10)
-                })
-           }
+                }).padding(EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0))
+            }
+            .background(Color.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 50))
+            //.overlay(RoundedRectangle(cornerRadius: 100).stroke(Color.black))
+            .background(RoundedRectangle(cornerRadius: 50)
+                .stroke()
+            )
+            .padding(EdgeInsets(top: 8, leading: 7, bottom: 8, trailing: 17))
+            .shadow(color: .black, radius: 5)
         }.background(.cyan)
     }.navigationBarHidden(true)
   }
