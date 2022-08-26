f6491e01080ad16951bcc52ca2a16e0846a1728f
Matheus-C-S-Azevedo <matheus.costa.s.azevedo@gmail.com>
2022-08-26 15:35:22 -0400

    sprite movendo com offset em relação ao touch

--- Cooper/DraggableNode.swift
+++ Cooper/DraggableNode.swift
@@ -13,13 +13,15 @@ class DraggableNode: SKNode {
     
     var sprite: SKSpriteNode
     
-    var firstTouchPos: CGPoint
-    var secondTouchPos: CGPoint
+    var firstTouchPos: CGPoint = CGPoint(x: 0, y: 0)
+    var secondTouchPos: CGPoint = CGPoint(x: 0, y: 0)
+    // Variável para verificar se o sprite está sendo rotacionado
+    var isRotating:Bool = false
+    
+    var touchOffset:CGPoint = CGPoint(x: 0, y: 0)
     
     override init() {
         self.sprite = SKSpriteNode()
-        firstTouchPos = CGPoint(x: 0, y: 0)
-        secondTouchPos = CGPoint(x: 0, y: 0)
         super.init()
         
         self.zPosition = 0
@@ -28,57 +30,64 @@ class DraggableNode: SKNode {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Habilita multiplos toques na tela.
         self.scene?.view?.isMultipleTouchEnabled = true
+        // Traz o sprite para a camada mais em cima.
         layerCount += 1
         self.zPosition = CGFloat(layerCount)
+        // Quando inicia o toque reseta a variável isRotating.
+        isRotating = false
+        // Armazena a posicção do primeiro toque em relação com o sprite.
+        if touches.count == 1{
+            touchOffset.x = (touches.first?.location(in: scene!).x)! - self.position.x
+            touchOffset.y = (touches.first?.location(in: scene!).y)! - self.position.y
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // Decide qual o primeiro e segundo toque.
         for touch in touches{
             let scene = self.scene
             let location = touch.location(in: scene!)
-            print(self.zRotation)
-            
             if touch == touches.first{
                 firstTouchPos = location
             }else{
                 secondTouchPos = location
             }
         }
-        print("primeiro toque: \(firstTouchPos)\nsegundo toque: \(secondTouchPos)")
-
+        
+        // Se o numero de toque for maior que um, rotaciona o sprite.
         if touches.count != 1 {
+            // Acha o ponto médio entre os dois toques.
             let xCord = (firstTouchPos.x + secondTouchPos.x)/2
             let yCord = (firstTouchPos.y + secondTouchPos.y)/2
-            
-            let catetoOposto = secondTouchPos.y - firstTouchPos.y
-            let catetoAdjacente = secondTouchPos.x - firstTouchPos.x
-            
-            let tangente = catetoOposto / catetoAdjacente
-            let angulo = atan(tangente)
-            let angulo2 = angulo - self.zRotation
-            
-            let action = SKAction.rotate(byAngle: angulo2, duration: 0.5)
-            self.run(action)
-            
-            
+            // Move o sprite para o ponto médio dos dois toques.
             self.position = CGPoint(x: xCord, y: yCord)
             
+            // Calcula anglo entre os dois toques.
+            let catetoOposto = secondTouchPos.y - firstTouchPos.y
+            let catetoAdjacente = secondTouchPos.x - firstTouchPos.x
+//            let hipotenusa = sqrt(pow(catetoOposto,2) + pow(catetoAdjacente, 2))
+            let tangente = catetoOposto / catetoAdjacente
+            let angulo = atan(tangente)
+            // Calcula a distância entre o angulo dos toques e o angulo atual do sprite.
+            let angulo2 = angulo - self.zRotation
+            // Adiciona o angulo necessário no angulo do sprite.
+            self.zRotation += angulo2
+//            self.setScale(hipotenusa / 320)
+            // Se o sprite tá rotacionando não deixa ele ir para o firstTouchPos
+            isRotating = true
             
-            
+        }else if (touches.count == 1 && isRotating == false) {
+            // Move o sprite para a posição do dedo, levando em conta o touchOffset.
+            self.position = CGPoint(x: (firstTouchPos.x - touchOffset.x), y: (firstTouchPos.y - touchOffset.y))
         }
-//        if let touch = touches.first, let scene = self.scene {
-//            let touchLocation = touch.location(in: scene)
-//            self.position = touchLocation
-//        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        self.zPosition = 0
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        self.zPosition = 0
     }
     
     
