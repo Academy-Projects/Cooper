12d1bb1a4fb1816d84b6da31a6a33371ab67a3ab
kevin marinho <kevindmarinho4321@gmail.com>
2022-09-05 23:26:36 -0400

    insert font and autolayout answer final

--- Cooper/View/AnswerFinalView.swift
+++ Cooper/View/AnswerFinalView.swift
@@ -10,72 +10,212 @@ import SpriteKit
 struct AnswerFinalView: View {
     
     var ilustrationScene: GameScene
+    var story: ListHistory = naps[0]
+    @State var cor:Color = Color(red: 0/255, green: 59/255, blue: 75/255)
+    
+    @State var buttonPressed:Int = 0
+    @State var opacityButton:Double = 1
+    @State var opacityPlay: Double = 0
     
     var body: some View{
-        ZStack{
-//            background(Color.gray)
-            
+      ZStack{
             HStack{
-                //adiciona um screenshot da Illustration scene
-                Image(uiImage: ilustrationScene.takeScreenshot(scene: ilustrationScene))
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 700, height: 700, alignment: .center)
-                    .border(Color.black, width: 10)
-                VStack{
-                    Button(action: {ilustrationScene.removeAllChildren()},
-                           label: {
-                            Text("Option 1")
-                            .foregroundColor(.white)
-                            .padding(.vertical, 50)
-                            .padding(.horizontal, 200)
-                            .background(Color.blue.cornerRadius(20))
-                    })
-                    Button(action: {ilustrationScene.removeAllChildren()},
-                           label: {
-                            Text("Option 2")
-                            .foregroundColor(.white)
-                            .padding(.vertical, 50)
-                            .padding(.horizontal, 200)
-                            .background(Color.blue.cornerRadius(20))
-                    })
-                    Button(action: {ilustrationScene.removeAllChildren()},
-                           label: {
-                            Text("Option 3")
-                            .foregroundColor(.white)
-                            .padding(.vertical, 50)
-                            .padding(.horizontal, 200)
-                            .background(Color.blue.cornerRadius(20))
-                    })
+                ZStack{
+                    //adiciona um screenshot da Illustration scene
+
+                    Image(uiImage: ilustrationScene.takeScreenshot(scene: ilustrationScene))
+                        .resizable()
+                        .scaledToFit()
+                    ZStack{
+                        HStack{
+                                NavigationLink(destination: DrawnView(), label: {
+                                    Rectangle()
+                                    Text("<")
+                                        .frame(width: UIScreen.main.bounds.width * 0.026, height: UIScreen.main.bounds.height * 0.040)
+                                        .background(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1))
+                                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                                        .background(RoundedRectangle(cornerRadius: 12)
+                                            .stroke(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1), lineWidth: 1)
+                                        )
+                                }).frame(width: UIScreen.main.bounds.width * 0.026, height: UIScreen.main.bounds.height * 0.040)
+                                
+                        }.frame(maxWidth: .infinity, alignment: .leading)
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .padding(.leading, 24)
+                        .padding(.top, 24)
+                    }
+                    .frame(width: UIScreen.main.bounds.width * 0.65, height: UIScreen.main.bounds.height * 0.87)
+                    .padding(EdgeInsets(top: 12, leading: 12, bottom: 12, trailing: 0))
+                    .border(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1), width: 2)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .background(RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1), lineWidth: 3)
+                        )
                 }
+                VStack{
+                    Text("O que Cooper está \ntentando comunicar?")
+                        .bold()
+                        .scaledToFit()
+                        .font(Font.custom("SourceSans3-Bold", size: 20))
+                        .foregroundColor(Color("colorFont"))
+                        .multilineTextAlignment(.center)
+                        .padding(EdgeInsets(top: 25, leading: 0, bottom: 14, trailing: 0))
+                    
+                    Button(action: {
+                                buttonPressed = 1
+                                opacityButton = 0.1
+                                opacityPlay = 1
+                            },
+                           label: {
+                           
+                                if buttonPressed == 1{
+                                    Text(story.finalOne)
+                                        .font(Font.custom("SourceSans3-Bold", size: 20))
+                                        .frame(width: UIScreen.main.bounds.width * 0.22)
+                                        
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(cor)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .background(RoundedRectangle(cornerRadius: 8)
+                                            .stroke(Color.clear.opacity(12.2), lineWidth: 1)
+                                        )
+                                    
+                                } else{
+                                    Text(story.finalOne)
+                                        .font(Font.custom("SourceSans3-Bold", size: 20))
+                                        .frame(width: UIScreen.main.bounds.width * 0.22)
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(Color(red: 0/255, green: 59/255, blue: 75/255))
+                                        .opacity(opacityButton)
+                                        .animation(.easeInOut(duration: 0.5))
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .background(RoundedRectangle(cornerRadius: 8)
+                                            .stroke(Color.clear.opacity(12.2), lineWidth: 1)
+                                        )
+                                }
+                            }).frame(width: UIScreen.main.bounds.width * 0.8)
+            //        Spacer()
+                    Button(action: {
+                            
+                            buttonPressed = 2
+                            opacityButton = 0.1
+                            opacityPlay = 1
+                            },
+                           label: {
+                                if buttonPressed == 2{
+                                    Text(story.finalTwo)
+                                        .font(Font.custom("SourceSans3-Bold", size: 20))
+                                        .frame(width: UIScreen.main.bounds.width * 0.22)
+                                        .multilineTextAlignment(.center)
+                                        .font(.system(size: 20))
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(cor)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .background(RoundedRectangle(cornerRadius: 8)
+                                            .stroke(Color.clear.opacity(12.2), lineWidth: 1)
+                                        )
+                                } else{
+                                    Text(story.finalTwo)
+                                        .font(Font.custom("SourceSans3-Bold", size: 20))
+                                        .frame(width: UIScreen.main.bounds.width * 0.22)
+                                        .multilineTextAlignment(.center)
+                                        .font(.system(size: 20))
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(Color(red: 0/255, green: 59/255, blue: 75/255))
+                                        .opacity(opacityButton)
+                                        .animation(.easeInOut(duration: 0.5))
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .background(RoundedRectangle(cornerRadius: 8)
+                                            .stroke(Color.clear.opacity(12.2), lineWidth: 1)
+                                        )
+                                }
+                            }).frame(width: UIScreen.main.bounds.width * 0.8)
+                   // Spacer()
+                    Button(action: {
+                            
+                            buttonPressed = 3
+                            opacityButton = 0.1
+                            opacityPlay = 1
+                            },
+                           label: {
+                                if buttonPressed == 3{
+                                    Text(story.finalThree)
+                                        .font(Font.custom("SourceSans3-Bold", size: 20))
+                                        .frame(width: UIScreen.main.bounds.width * 0.22)
+                                        .multilineTextAlignment(.center)
+                                        .font(.system(size: 20))
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(cor)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .background(RoundedRectangle(cornerRadius: 8)
+                                            .stroke(Color.clear.opacity(12.2), lineWidth: 1)
+                                        )
+                                } else{
+                                    Text(story.finalThree)
+                                        .font(Font.custom("SourceSans3-Bold", size: 20))
+                                        .frame(width: UIScreen.main.bounds.width * 0.22)
+                                        .multilineTextAlignment(.center)
+                                        .font(.system(size: 20))
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(Color(red: 0/255, green: 59/255, blue: 75/255))
+                                        .opacity(opacityButton)
+                                        .animation(.easeInOut(duration: 0.5))
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .background(RoundedRectangle(cornerRadius: 8)
+                                            .stroke(Color.clear.opacity(12.2), lineWidth: 1)
+                                        )
+                                }
+                            }).frame(width: UIScreen.main.bounds.width * 0.8)
+                    
+             
+                }.frame(width: UIScreen.main.bounds.width * 0.26)
+                    .background(Color.white)
+                    .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 17))
             }
-        }.background(.gray)
-    }
-}
+            .frame(width: UIScreen.main.bounds.width * 0.96, height: UIScreen.main.bounds.height * 0.92)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 13))
+            .shadow(color: Color(red: 232/255, green: 232/255, blue: 232/255, opacity: 85), radius: 10)
+            //.overlay(RoundedRectangle(cornerRadius: 100).stroke(Color.black))
+            .background(RoundedRectangle(cornerRadius: 13)
+            ).padding()
+            
+      }.frame(maxWidth: .infinity, maxHeight: .infinity)
+       .background(LinearGradient(gradient: Gradient(colors: [Color.white, Color("backgroundIlustration"), Color("backgroundIlustration"), Color.white]),
+                                        startPoint: .leading,
+                                        endPoint: .trailing))
+         .navigationBarHidden(true)
 
-struct SampleDetails: View {
-    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-
-    var btnBack : some View {
-        Button(action: {
-        self.presentationMode.wrappedValue.dismiss()
-        }) {
-            Rectangle()
-            Text("")
-                .frame(width: UIScreen.main.bounds.width * 0.026, height: UIScreen.main.bounds.height * 0.040)
-                .background(Color.gray.opacity(0.2))
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .background(RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color.gray.opacity(0.2), lineWidth: 1)
-                )
-        }.padding(.leading, 600)
     }
     
-    var body: some View {
-            List {
-                Text("sample code")
+    
+}
+
+
+
+struct FrameAdjustingContainer<Content: View>: View {
+    @Binding var frameWidth: CGFloat
+    @Binding var frameHeight: CGFloat
+    let content: () -> Content
+    
+    var body: some View  {
+        ZStack {
+            content()
+                .frame(width: frameWidth, height: frameHeight)
+                .border(Color.red, width: 1)
+            
+            VStack {
+                Spacer()
+                Slider(value: $frameWidth, in: 50...300)
+                Slider(value: $frameHeight, in: 50...600)
+            }
+            .padding()
         }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: btnBack)
     }
 }
