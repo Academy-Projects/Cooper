a2b0b3e7f4cf068067fb24e85ee359a62dafd2f6
kevin marinho <kevindmarinho4321@gmail.com>
2022-09-07 19:36:57 -0400

    adicionando funcionalidade na onboarding

--- Cooper/View/OnBoardingView.swift
+++ Cooper/View/OnBoardingView.swift
@@ -7,60 +7,87 @@
 
 import SwiftUI
 
+struct OnboardingStep{
+    let image: String
+    let isntruction: String
+}
+
+private let OnboardingSteps = [
+    OnboardingStep(image: "blocked", isntruction: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"),
+    OnboardingStep(image: "blocked", isntruction: "qualquer outro texto"),
+    OnboardingStep(image: "blocked", isntruction: "qualquer outro texto de novo")
+]
+
 struct OnBoardingView: View {
+    
+    @State private var currentStep = 0
+    
     var body: some View {
         ZStack{
+            TabView(selection: $currentStep){
+                ForEach(0..<OnboardingSteps.count){ it in
             VStack{
-                Image("blocked")
-                    .resizable()
-                    .frame(width: UIScreen.main.bounds.width * 0.69, height: UIScreen.main.bounds.height * 0.49)
-                    .padding(.bottom, 24)
-                    .padding(.top, 64)
-                
-                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s")
-                    .frame(width: UIScreen.main.bounds.width * 0.49)
-                    .foregroundColor(Color("colorFont"))
-                    //.padding(.bottom, 20)
-                    .font(Font.custom("SourceSans3-Bold", size: 23))
-                    .multilineTextAlignment(.center)
-                
-                Spacer()
-                
-                HStack(spacing: 0){
-                    Button(action:{} , label: {Text("Pular")})
-                        .font(Font.custom("SourceSans3-Regular", size: 22))
-                        .foregroundColor(Color("colorFont"))
-                    
-                     //   .frame(width: UIScreen.main.bounds.width * 0.08)
-            
-                    Spacer()
-                    Button(action:{} , label: {Text("...")})
-                        // .frame(width: UIScreen.main.bounds.width * 0.08)
-                    Spacer()
-                    Button(action:{} ,
-                           label:{
-                        Image(systemName: "chevron.right")
+                        Image(OnboardingSteps[it].image)
                             .resizable()
-                            .scaledToFit()
-                            .padding(.vertical, 6)
-                            .font(Font.custom("SourceSans3-Regular", size: 10))
+                            .frame(width: UIScreen.main.bounds.width * 0.69, height: UIScreen.main.bounds.height * 0.49)
+                            .padding(.bottom, 24)
+                            .padding(.top, 64)
+                        
+                        Text(OnboardingSteps[it].isntruction)
+                            .frame(width: UIScreen.main.bounds.width * 0.49)
                             .foregroundColor(Color("colorFont"))
-                            .frame(width: UIScreen.main.bounds.width * 0.026, height: UIScreen.main.bounds.height * 0.040)
-                            .background(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1))
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                            .background(RoundedRectangle(cornerRadius: 12)
-                                .stroke(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1), lineWidth: 1)
-                            )
+                            //.padding(.bottom, 20)
+                            .font(Font.custom("SourceSans3-Bold", size: 23))
+                            .multilineTextAlignment(.center)
                         
-                    })
-                      //  .frame(width: UIScreen.main.bounds.width * 0.08)
+                        Spacer()
                         
+                        HStack(spacing: 0){
+                            Button(action:{
+                                self.currentStep = OnboardingSteps.count - 1
+                            } , label: {Text("Pular")})
+                                .font(Font.custom("SourceSans3-Regular", size: 22))
+                                .foregroundColor(Color("colorFont"))
+                    
+                            Spacer()
+                            
+                            Button(action:{} , label: {Text("...")})
+                            
+                            Spacer()
+                            
+                            Button(action:{
+                                if self.currentStep < OnboardingSteps.count - 1{
+                                    self.currentStep += 1
+                                } else{
+                                    //nothing
+                                }
+                            } ,
+                                   label:{
+                                Image(systemName: "chevron.right")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(.vertical, 6)
+                                    .font(Font.custom("SourceSans3-Regular", size: 10))
+                                    .foregroundColor(Color("colorFont"))
+                                    .frame(width: UIScreen.main.bounds.width * 0.026, height: UIScreen.main.bounds.height * 0.040)
+                                    .background(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1))
+                                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                                    .background(RoundedRectangle(cornerRadius: 12)
+                                        .stroke(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1), lineWidth: 1)
+                                    )
+                                
+                            })
+                              //  .frame(width: UIScreen.main.bounds.width * 0.08)
+                                
+                        }
+                        .frame(maxWidth: .infinity)
+                        .padding(.bottom, 15)
+                        
+                    }.tag(it)
+                    
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.bottom, 15)
-
-                
             }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             .frame(width: UIScreen.main.bounds.width * 0.93, height: UIScreen.main.bounds.height * 0.89)
             .padding()
             .border(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1), width: 2)
@@ -68,7 +95,8 @@ struct OnBoardingView: View {
             .background(RoundedRectangle(cornerRadius: 20)
                 .stroke(Color(red: 254/255, green: 179/255, blue: 18/255, opacity: 1), lineWidth: 3)
                 )
-        }.navigationBarHidden(true)
+        }
+        .navigationBarHidden(true)
     }
 }
 
