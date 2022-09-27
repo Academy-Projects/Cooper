1f86ecb1fd6bef7c4afe1f0d242d033eef94bbe6
Matheus-C-S-Azevedo <matheus.costa.s.azevedo@gmail.com>
2022-09-27 13:34:13 -0400

    class da animationScene criada

---
+++ Cooper/Model/AnimatinoScene.swift
@@ -0,0 +1,21 @@
+//
+//  AnimatinoScene.swift
+//  Cooper
+//
+//  Created by Matheus Costa on 27/09/22.
+//
+
+import Foundation
+import SwiftUI
+import SpriteKit
+
+class AnimationScene: SKScene{
+    
+    override func sceneDidLoad() {
+        self.sceneDidLoad()
+        self.backgroundColor = .clear
+    }
+    
+    override func didMove(to view: SKView) {
+    }
+}
