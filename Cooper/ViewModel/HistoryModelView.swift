212045884d4308842a0a06e0806f9e3def640527
kevin marinho <kevindmarinho4321@gmail.com>
2022-10-10 20:34:13 -0400

    refatorando historyView

---
+++ Cooper/ViewModel/HistoryModelView.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryModelView.swift
+//  Cooper
+//
+//  Created by kevin marinho on 10/10/22.
+//
+
+import SwiftUI
+
+struct HistoryModelView{
+    var history = History(
+        idx: 0,
+        titulo: ["FRANÇA","GRÉCIA","BRASIL","LONDRES", "CANADÁ", "PERU"],
+        card: ["CardParis-8","CardGrecia.DarkMode","CardAmazonia.DarkMode", "cardlondres", "cardcanada", "cardperu", "blocked", "blocked", "blocked", "blocked"],
+        presentResultAlert: false,
+        result: false,
+        personagem: "PERSONAGEM"
+    )
+}
+
+
